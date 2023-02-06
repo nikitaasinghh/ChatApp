5d78c275412c496d231e33c1810b2802bb2d2e2d
Sabyasachi <yoboy907@gmail.com>
2023-02-06 10:14:11 +0530

    Minor Test Fixes

--- jsFile/client.js
+++ jsFile/client.js
@@ -1,45 +1,75 @@
-const socket = io('http://localhost:8000');
-
-const form = document.getElementById('send-container');
-const messageInput = document.getElementById('msgInpt');
-const messageContainer = document.querySelector('.container');
-
-
-var audio = new Audio('ting.mp3');
-
-//  notification -- when someone joins
-const append = (message, position) => {
-    const msgElm = document.createElement('div')
-    msgElm.innerText = message;
-    msgElm.classList.add('message');
-    msgElm.classList.add(position);
-    messageContainer.append(msgElm);
-    if (position == 'left'){
-        audio.play();
-    }
-}
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();    //avoids reloading
-    const message = messageInput.value;
-    append(`You: ${message}`, 'right');
-    socket.emit('send', message);
-    messageInput.value = '';
-})
-
-
+const socket = io("http://localhost:8000");
+const button = document.getElementById("test");
+const messageContainer = document.querySelector(".container");
+var audio = new Audio("ting.mp3");
 
 const name = prompt("Enter your name to join");
-socket.emit('new-user-joined', name);
+socket.emit("new-user-joined", name);
 
-socket.on('user-joined', name => {
-    append(`${name} joined the chat`, 'center');
-})
+//  notification -- when someone joins
 
-socket.on('receive', data => {
-    append(`${data.name}: ${data.message}`, 'left')
-})
+button.addEventListener("click", (e) => {
+    e.preventDefault(); //avoids reloading
+    console.log("hello");
+    setTimeout(putForm, 1000);
+    addEL();
+});
 
-socket.on('left', name => {
-    append(`${name} left the chat`, 'center')
-})
+async function putForm() {
+    var str =
+        '<form action="#" class="send-container">' +
+        '<input type="text" name="msgInp" id="msgInpt" placeholder="We promise to keep it a secret..!!">' +
+        '<button type="submit" class="btn">Send</button>' +
+        "</form>";
+
+
+    var Obj = document.getElementById("test"); 
+
+
+    if (Obj.outerHTML) {
+        Obj.outerHTML = str; 
+    } 
+
+}
+
+function sleepFor(sleepDuration){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){ /* Do nothing */ }
+}
+
+function addEL() {
+
+    const form = document.getElementById("send-container");
+    const messageInput = document.getElementById("msgInpt");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault(); //avoids reloading
+        const message = messageInput.value;
+        append(`You: ${message}`, "right");
+        socket.emit("send", message);
+        messageInput.value = "";
+    });
+
+}
+
+const append = (message, position) => {
+    const msgElm = document.createElement("div");
+    msgElm.innerText = message;
+    msgElm.classList.add("message");
+    msgElm.classList.add(position);
+    messageContainer.append(msgElm);
+    if (position == "left") {
+        audio.play();
+    }
+};
+
+socket.on("user-joined", (name) => {
+    append(`${name} joined the chat`, "center");
+});
+
+socket.on("receive", (data) => {
+    append(`${data.name}: ${data.message}`, "left");
+});
+
+socket.on("left", (name) => {
+    append(`${name} left the chat`, "center");
+});
